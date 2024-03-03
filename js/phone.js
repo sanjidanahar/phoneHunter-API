e165239b36f711fd2bb5b8855ab0fabe33cec889
sanjidanahar <cse02707453@gmail.com>
2024-03-03 22:57:34 +0600

    phone hunter api

---
+++ js/phone.js
@@ -0,0 +1,43 @@
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(data);
+    // console.log(data.data);
+    // console.log(phones);
+    displayPhones(phones);
+}
+
+// step 1
+const displayPhones = phones => {
+    // console.log(phone)
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone);
+
+        // 2 create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
+       
+    //    3: set inner HTML
+        phoneCard.innerHTML = ` <figure><img src="${phone.image}"
+        alt="Shoes"/></figure>
+<div class="card-body">
+    <h2 class="card-title">${phone.phone_name}</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+    </div>
+</div>`;
+
+// 4: append child
+phoneContainer.appendChild(phoneCard);
+    })
+}
+
+
+// handle search button
+const handleSearch = () => {
+    console.log('search handle')
+}
+loadPhone();
