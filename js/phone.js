6fc3f8d9e1d0cf76e4894126b3b886ffdccacbc3
sanjidanahar <cse02707453@gmail.com>
2024-03-07 00:29:57 +0600

    loader API added

--- js/phone.js
+++ js/phone.js
@@ -14,22 +14,22 @@ const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container');
 
     //clear phone container cards before adding new cards
-    phoneContainer.textContent =''; 
+    phoneContainer.textContent = ''; 
 
     // console.log(phones.length);
 
     // display show all button if there are more than 12 button
     const showAllContainer = document.getElementById('show-all-container');
-    if(phones.length > 12){
+    if (phones.length > 12) {
         showAllContainer.classList.remove('hidden')
     }
-    else{
+    else {
         showAllContainer.classList.add('hidden')
     }
 
 
     // display only first 12 phones
-    phones = phones.slice(0,12);
+    phones = phones.slice(0, 12);
 
     phones.forEach(phone => {
         // console.log(phone);
@@ -37,21 +37,24 @@ const displayPhones = phones => {
         // 2 create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
-       
-    //    3: set inner HTML
+
+        //    3: set inner HTML
         phoneCard.innerHTML = ` <figure><img src="${phone.image}"
         alt="Shoes"/></figure>
-<div class="card-body">
-    <h2 class="card-title">${phone.phone_name}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
-    </div>
-</div>`;
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>`;
 
-// 4: append child
-phoneContainer.appendChild(phoneCard);
-    })
+        // 4: append child
+        phoneContainer.appendChild(phoneCard);
+    });
+
+    // hide loading spinner
+    toggleLoadingBars(false);
 }
 
 
@@ -60,16 +63,30 @@ phoneContainer.appendChild(phoneCard);
 // handle search button
 const handleSearch = () => {
     // console.log('search handle');
+    toggleLoadingBars(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
     loadPhone(searchText);
 }
 
+// handle search recap
 const handleSearch2 = () => {
+    toggleLoadingBars(true);
     const searchField = document.getElementById('search-field2');
     const searchText = searchField.value;
     loadPhone(searchText);
+}
+
+// loading bar 
+const toggleLoadingBars = (isLoading) => {
+    const loadingBars = document.getElementById('loading-bars');
+    if(isLoading){
+        loadingBars.classList.remove('hidden')
+    }
+    else{
+        loadingBars.classList.add('hidden');
+    }
 }
 
 
